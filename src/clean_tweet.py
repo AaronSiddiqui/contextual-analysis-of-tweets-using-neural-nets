998016fc14900ff1e7c50b5ee5e731ad6ec02e33
Aaron Siddiqui <aaronboyle1@gmail.com>
2020-02-21 19:56:43 +0000

    Further parsed the tweet to remove emojis and punctuation and expand abbreviation

--- src/clean_tweet.py
+++ src/clean_tweet.py
@@ -3,47 +3,65 @@ import re
 import random
 import string
 
-"""Parses and cleans the tweet with a variety of options"""
-def clean_tweet(tweet, remove_urls=True, remove_mentions=True,
-                replace_mentions=False, remove_hashtags=True, decode_html=True,
-                replace_nonprintable_chars=True, convert_lowercase=True,
-                remove_whitespace=True):
+"""Parses and cleans the tweet"""
+def clean_tweet(tweet, rem_urls=True, rem_mentions=True, rem_texemojis=True,
+                repl_mentions=False, rem_htags=True, dec_html=True,
+                conv_lcase=True, expand_abbrev=True, rem_punc=True,
+                repl_nonprint_chars=True, rem_wspace=True):
     # The tweet can either remove @mentions or replace them
-    if remove_mentions and replace_mentions:
+    if rem_mentions and repl_mentions:
         print("You can only choose one of them")
         return
 
     # Remove the urls
-    if remove_urls:
-        tweet = re.sub(r"https?:\S+", "", tweet)
+    if rem_urls:
+        tweet = re.sub(r"((https?:)|(www.))\S+", " ", tweet)
 
     # Removes or replaces the @mentions
-    if remove_mentions:
-        tweet = re.sub(r"@\S+", "", tweet)
-    elif replace_mentions:
+    if rem_mentions:
+        tweet = re.sub(r"@\S+", " ", tweet)
+    elif repl_mentions:
         names = ["Tom", "Tim", "Sam", "Matthew", "John"]
-        return re.sub(r"@\S+", random.choice(names), tweet)
+        tweet = re.sub(r"@\S+", " " + random.choice(names) + " ", tweet)
+
+    # Remove basic text-based emojis
+    if rem_texemojis:
+        tweet = re.sub(r" [:;Xx=][038DdPpSsL<>/\\\(\)\[\]\{\}\-]{1,2}", " ", tweet)
 
     # Removes the hashtags
-    if remove_hashtags:
-        tweet = re.sub(r"#\S+", "", tweet)
+    if rem_htags:
+        tweet = re.sub(r"#\S+", " ", tweet)
 
     # Decodes the html
-    if decode_html:
+    if dec_html:
         tweet = unescape(tweet)
 
+    # Converts to lower case
+    if conv_lcase:
+        tweet = tweet.lower()
+
+    # Expands abbreviations
+    if expand_abbrev:
+        abbrevs = {"can't": "can not", "won't": "will not", "n't": " not",
+                   "'ve": " have", "'re": " are", " it's": " it is",
+                   "i'm": "i am", " he's": " he is", " she's": " she is",
+                   "'ll": " will", "'d": " would", "&": " and ", "%": " percent"}
+        for k, v in abbrevs.items():
+            tweet = re.sub(r"" + k, v, tweet)
+
+    # Removes punctuation
+    if rem_punc:
+        tweet = tweet.translate(str.maketrans("", "", string.punctuation))
+
     # Replaces not printable character with "?" i.e. UTF-8 BOM characters
-    if replace_nonprintable_chars:
+    if repl_nonprint_chars:
         for char in tweet:
             if char not in string.printable:
                 tweet = tweet.replace(char, "?")
 
-    # Converts to lower case
-    if convert_lowercase:
-        tweet = tweet.lower()
-
-    # Removes white space
-    if remove_whitespace:
-        tweet = " ".join(tweet.split())
+    # Removes whitespace
+    if rem_wspace:
+        tweet = re.sub(r"(\s{2,})|(\t+)", " ", tweet)
+        tweet = re.sub(r"(^\s+)|(\s+$)", "", tweet)
 
     return tweet
