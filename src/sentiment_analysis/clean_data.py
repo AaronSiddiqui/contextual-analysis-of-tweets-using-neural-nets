37fc96c0efed677b548a0de37e2a8d862f22e1cf
Aaron Siddiqui <a.siddiqui1@nuigalway.ie>
2020-03-08 18:58:36 +0000

    Removed tweets that are null in the cleaning process

--- src/sentiment_analysis/clean_data.py
+++ src/sentiment_analysis/clean_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from src.clean_tweet import clean_tweet
 
@@ -39,8 +40,12 @@ print("Hashtag Example:", clean_df.text[175])
 print("HTML Encoding Example:", clean_df.text[279])
 print("UTF-8 BOM Example:", clean_df.text[226])
 
-# Prints tweets that are null after the data is processed
+# Replaces tweets with empty strings after being processed as null so they
+# can be removed
 # e.g. A tweet that only had a url
+clean_df.text.replace("", np.nan, inplace=True)
+
+# Prints tweets that are null
 print()
 print(clean_df[clean_df.isnull().any(axis=1)].head())
 
