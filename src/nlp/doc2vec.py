fc8ece7acbf29c27171f97d827e44a2705a4d5a2
Aaron Siddiqui <aaronboyle1@gmail.com>
2020-03-11 19:14:49 +0000

    Removed the save_model functions and altered the create_model functions to include default options and return the model

--- src/nlp/doc2vec.py
+++ src/nlp/doc2vec.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 from sklearn import utils
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import multiprocessing
 
 
 class TaggedCorpus:
@@ -19,19 +20,22 @@ class TaggedCorpus:
 class D2V:
     def __init__(self, corpus, **opts):
         self.corpus = TaggedCorpus(corpus)
-        self.model = Doc2Vec(**opts)
 
-    def create_model(self, epochs, learning_rate):
-        self.model.build_vocab(self.corpus)
+    def create_model(self, dm, epochs, learning_rate):
+        default_opts = {"vector_size": 64,
+                        "negative": 5,
+                        "min_count": 1,
+                        "workers": multiprocessing.cpu_count(),
+                        "alpha": 0.065,
+                        "min_alpha": 0.065}
+
+        model = Doc2Vec(dm=dm, **default_opts)
+        model.build_vocab(self.corpus)
 
         for _ in tqdm(range(epochs)):
-            self.model.train(utils.shuffle(self.corpus.to_array()),
-                             total_examples=len(self.corpus.to_array()),
-                             epochs=1)
-            self.model.min_alpha = self.model.alpha - learning_rate
+            model.train(utils.shuffle(self.corpus.to_array()),
+                        total_examples=len(self.corpus.to_array()),
+                        epochs=1)
+            model.min_alpha = model.alpha - learning_rate
 
-        return self.model
-
-    def save_model(self, name):
-        self.model.save(name)
-        return self.model
+        return model
--- src/nlp/word2vec.py
+++ src/nlp/word2vec.py
@@ -1,24 +1,29 @@
 from tqdm import tqdm
 from sklearn import utils
 from gensim.models import Word2Vec
+import multiprocessing
 import nltk
 
 
 class W2V:
-    def __init__(self, corpus, **opts):
+    def __init__(self, corpus):
         self.corpus = [nltk.word_tokenize(sen) for sen in tqdm(corpus)]
-        self.model = Word2Vec(**opts)
 
-    def create_model(self, epochs, learning_rate):
-        self.model.build_vocab(self.corpus)
+    def create_model(self, sg, epochs, learning_rate):
+        default_opts = {"size": 64,
+                        "negative": 5,
+                        "window": 2,
+                        "min_count": 1,
+                        "workers": multiprocessing.cpu_count(),
+                        "alpha": 0.065,
+                        "min_alpha": 0.065}
+
+        model = Word2Vec(sg=sg, **default_opts)
+        model.build_vocab(self.corpus)
 
         for _ in tqdm(range(epochs)):
-            self.model.train(utils.shuffle(self.corpus),
-                             total_examples=len(self.corpus), epochs=1)
-            self.model.min_alpha = self.model.alpha - learning_rate
+            model.train(utils.shuffle(self.corpus),
+                        total_examples=len(self.corpus), epochs=1)
+            model.min_alpha = model.alpha - learning_rate
 
-        return self.model
-
-    def save_model(self, name):
-        self.model.save(name)
-        return self.model
+        return model
