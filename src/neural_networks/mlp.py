b15d9c87d6f107939e1b98ec37ec829bc9f9e6a7
Aaron Siddiqui <a.siddiqui1@nuigalway.ie>
2020-03-24 16:59:15 +0000

    Renamed it more appropriately as its a multilayer perceptron network

--- src/neural_networks/mlp.py
+++ src/neural_networks/mlp.py
@@ -6,7 +6,7 @@ from src.neural_networks.train import train_nn
 """Returns a basic ANN with embedding"""
 
 
-def ann_01(model_path, x_train, y_train, x_val, y_val, in_dim, out_dim, in_len,
+def mlp_01(model_path, x_train, y_train, x_val, y_val, in_dim, out_dim, in_len,
            emb_opts):
     model = Sequential()
     model.add(Embedding(in_dim, out_dim, input_length=in_len, **emb_opts))
