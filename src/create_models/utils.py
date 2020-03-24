1fd1a369a8198b09aa19d47fbcb8e3f5a16dc8da
Aaron Siddiqui <a.siddiqui1@nuigalway.ie>
2020-03-24 17:01:27 +0000

    Changed the create_embedding_matrix to work for only for one word embedding map, changed the create_models_to_analyse function to create networks for Word2Vec CBOW and SG as opposed to a concatanetation of the two or Doc2Vec, and a few naming adjustments

--- src/create_models/utils.py
+++ src/create_models/utils.py
@@ -16,15 +16,14 @@ def create_dir_if_nonexist(dir_):
    models"""
 
 
-def create_embedding_matrix(word_emb_map1, word_emb_map2, num_words, vec_size,
+def create_embedding_matrix(word_emb_map, num_words, vec_size,
                             word_index):
     emb_index = {}
     emb_matrix = np.zeros((num_words, vec_size))
 
-    # Concatenates 2 word embedding mappings
-    # e.g. w2v_cbow + w2v_sg
-    for w in word_emb_map1.vocab.keys():
-        emb_index[w] = np.append(word_emb_map1[w], word_emb_map2[w])
+    # Creates an embedding index from the word embedding map
+    for w in word_emb_map.vocab.keys():
+        emb_index[w] = np.array(word_emb_map[w])
 
     # Creates an embedding matrix
     for w, i in word_index.items():
@@ -40,10 +39,10 @@ def create_embedding_matrix(word_emb_map1, word_emb_map2, num_words, vec_size,
 """Creates/loads and returns a vector model depending on it's existence"""
 
 
-def create_vec_model(model_type, model_path, vec_cls, **model_key_args):
+def create_vec_model(model_type, model_path, vec_cls, **model_kwargs):
     if not path.exists(model_path):
         print("Creating", model_type, "model:", model_path)
-        model = vec_cls.create_model(model_path, **model_key_args)
+        model = vec_cls.create_model(model_path, **model_kwargs)
     else:
         print(model_type, "model is already created:", model_path)
         model = KeyedVectors.load(model_path)
@@ -55,11 +54,11 @@ def create_vec_model(model_type, model_path, vec_cls, **model_key_args):
    existence"""
 
 
-def create_nn_model(model_type, model_path, model_func, model_pos_args,
+def create_nn_model(model_type, model_path, model_func, model_posargs,
                     model_emb_args={}):
     if not path.exists(model_path):
         print("Creating", model_type, "model:", model_path)
-        model = model_func(model_path, *model_pos_args, model_emb_args)
+        model = model_func(model_path, *model_posargs, model_emb_args)
     else:
         print(model_type, "model is already created:", model_path)
         model = load_model(model_path)
@@ -70,31 +69,32 @@ def create_nn_model(model_type, model_path, model_func, model_pos_args,
 """Creates/loads and returns the 3 models that we are analysing with a given 
    neural network:
      - Neural network with basic embedding
-     - Neural network with Word2Vec
-     - Neural network with Doc2Vec"""
+     - Neural network with Word2Vec CBOW
+     - Neural network with Word2Vec SG"""
 
 
-def create_models_to_analyse(w2v_emb_matrix, d2v_emb_matrix, nn_type,
-                             model_path, model_func, model_pos_args):
+def create_models_to_analyse(w2v_cbow_emb_matrix, w2v_sg_emb_matrix, nn_type,
+                             model_path, model_func, model_posargs):
     model_emb_type = nn_type + " with basic embedding"
-    model_w2v_type = nn_type + " with Word2Vec"
-    model_d2v_type = nn_type + " with Doc2Vec"
-
     model_emb = create_nn_model(model_emb_type, model_path + "_emb.h5",
-                                model_func, model_pos_args)
+                                model_func, model_posargs)
 
     # Word2Vec embedding matrix is added to this model
-    model_w2v = create_nn_model(model_w2v_type, model_path + "_w2v.h5",
-                                model_func, model_pos_args,
-                                {"weights": [w2v_emb_matrix],
-                                 "trainable": True})
+    model_w2v_cbow_type = nn_type + " with Word2Vec CBOW"
+    model_w2v_cbow = create_nn_model(model_w2v_cbow_type,
+                                     model_path + "_w2v_cbow.h5",
+                                     model_func, model_posargs,
+                                     {"weights": [w2v_cbow_emb_matrix],
+                                      "trainable": True})
 
-    # Doc2Vec embedding matrix is added to this model
-    model_d2v = create_nn_model(model_d2v_type, model_path + "_d2v.h5",
-                                model_func, model_pos_args,
-                                {"weights": [d2v_emb_matrix],
-                                 "trainable": True})
+    # Word2Vec embedding matrix is added to this model
+    model_w2v_sg_type = nn_type + " with Word2Vec SG"
+    model_w2v_sg = create_nn_model(model_w2v_sg_type,
+                                   model_path + "_w2v_sg.h5",
+                                   model_func, model_posargs,
+                                   {"weights": [w2v_sg_emb_matrix],
+                                    "trainable": True})
 
     # Returns them as tuples (model_type, model)
-    return (model_emb_type, model_emb), (model_w2v_type, model_w2v), \
-           (model_d2v_type, model_d2v)
+    return (model_emb_type, model_emb), (model_w2v_cbow_type, model_w2v_cbow),\
+           (model_w2v_sg_type, model_w2v_sg)
