e0d38f2975233891d45d019d9e767c25be3eebfe
Aaron Siddiqui <a.siddiqui1@nuigalway.ie>
2020-03-24 14:41:03 +0000

    Added a comment

--- src/tweepy/tweepy_credentials.py
+++ src/tweepy/tweepy_credentials.py
@@ -5,6 +5,7 @@ CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
 cred_dir = "../credentials"
 
+# Reads them in from the credentials directory
 with open(cred_dir + "/access_tokens.txt") as f:
     ACCESS_TOKEN = f.readline().split("\t")[1].rstrip()
     ACCESS_TOKEN_SECRET = f.readline().split("\t")[1].rstrip()
